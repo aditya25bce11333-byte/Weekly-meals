061e1d50da307e0b1417676aa8fb7a4ff5dee249
aditya25bce11333-byte <aditya.25bce11333@vitbhopal.ac.in>
2026-02-10 02:05:59 +0530

    Add console log for version tracking

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log("version 2");
 const API_URL = "https://698a22b0c04d974bc6a17e23.mockapi.io/Menu"; // Replace with your actual API endpoint
 
 /* ---------- TIME & DAY HELPERS ---------- */
@@ -106,4 +107,5 @@ setInterval(loadMealByTime, 60000);
 
 /* ---------- BUTTON BINDING ---------- */
 document.getElementById("toggle-week").onclick = toggleWeeklyMenu;
+
 
